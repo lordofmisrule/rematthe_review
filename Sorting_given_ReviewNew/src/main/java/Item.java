dff958e2b9c9b14ef36352cdbeb574d4688ff323
lordofmisrule <rudyedeleon@gmail.com>
2018-04-10 20:54:36 -0700

    Update Item.java

--- Sorting_given_ReviewNew/src/main/java/Item.java
+++ Sorting_given_ReviewNew/src/main/java/Item.java
@@ -1,1 +1,17 @@
-package main.java;public final class Item {        public int key;        //constructor        public Item(int keyValue) {                 key = keyValue;        }        // further components and methods}
+package main.java;
+
+/**
+ * Item class that has a variable key of type int that is used to 
+ * define it
+ * @author Rudy
+ *
+ */
+
+public final class Item {
+        public int key;
+        //constructor
+        public Item(int keyValue) { 
+                key = keyValue;
+        }
+        // further components and methods
+}
